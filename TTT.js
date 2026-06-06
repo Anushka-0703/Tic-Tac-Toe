ad06f39e4bc23ef2bc91ebf13a105a6b72b548df
Anushka-0703 <anushka.970747@gmail.com>
2026-06-06 12:45:05 +0545

    completed functionality

--- TTT.js
+++ TTT.js
@@ -18,60 +18,60 @@ const winPatterns = [
      [0, 1, 2],
 ];
 
-// let checkWinner = () => {
-//      winPatterns.forEach((pattern) => {
-//           // console.log(pattern[0],pattern[1],pattern[2]);//[2,5,8]
+let checkWinner = () => {
+     winPatterns.forEach((pattern) => {
+          // console.log(pattern[0],pattern[1],pattern[2]);//[2,5,8]
 
-//           // console.log(boxes[pattern[0]].innerText, boxes[pattern[1]].innerText, boxes[pattern[2]].innerText);
-//           let pos1Val = boxes[pattern[0]].innerText;
-//           let pos2Val = boxes[pattern[1]].innerText;
-//           let pos3Val = boxes[pattern[2]].innerText;
+          // console.log(boxes[pattern[0]].innerText, boxes[pattern[1]].innerText, boxes[pattern[2]].innerText);
+          let pos1Val = boxes[pattern[0]].innerText;
+          let pos2Val = boxes[pattern[1]].innerText;
+          let pos3Val = boxes[pattern[2]].innerText;
 
-//           if (pos1Val && pos2Val && pos3Val) {
-//                if (pos1Val === pos2Val && pos2Val === pos3Val) {
-//                     showWinner(pos1Val);
-//                }
-//           }
-//      });
-// };
+          if (pos1Val && pos2Val && pos3Val) {
+               if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                    showWinner(pos1Val);
+               }
+          }
+     });
+};
 
-// function showWinner(winner) {
-//      // console.log(winner);
-//      container.style.display = "none";
-//      winContainer.style.display = "flex";
-//      msg.innerText = `Congratulation ! Player ${winner} won`
-// }
+function showWinner(winner) {
+     // console.log(winner);
+     container.style.display = "none";
+     winContainer.style.display = "flex";
+     msg.innerText = `Congratulation ! Player ${winner} won`
+}
 
-// boxes.forEach((box) => {
-//      box.addEventListener("click", (e) => {
-//           if (turnX) {
-//                box.innerText = "X";
-//                turnX = false;
-//           } else {
-//                box.innerText = "O";
-//                turnX = true;
-//           }
-//           box.disabled = true;
-//           checkWinner();
-//      });
-// });
+boxes.forEach((box) => {
+     box.addEventListener("click", (e) => {
+          if (turnX) {
+               box.innerText = "X";
+               turnX = false;
+          } else {
+               box.innerText = "O";
+               turnX = true;
+          }
+          box.disabled = true;
+          checkWinner();
+     });
+});
 
-// // console.log(boxes);
+// console.log(boxes);
 
-// resetBtn.addEventListener("click", reset);
-// newGame.addEventListener("click", () => {
-//      reset();
-//      container.style.display = "flex";
-//      winContainer.style.display = "none";
-// });
+resetBtn.addEventListener("click", reset);
+newGame.addEventListener("click", () => {
+     reset();
+     container.style.display = "flex";
+     winContainer.style.display = "none";
+});
 
-// function reset() {
-//      boxes.forEach((box) => {
-//           box.disabled = false;
-//           box.innerText = "";
-//      });
-//      turnX = true;
-// }
+function reset() {
+     boxes.forEach((box) => {
+          box.disabled = false;
+          box.innerText = "";
+     });
+     turnX = true;
+}
 
 //added new line
 
